e763bcd037c09874b2d99791a9afa61d9714dab4
farranfitz <farranfitz@gmail.com>
2017-05-01 06:35:41 +1200

    Added more.js

---
+++ more.js
@@ -0,0 +1,33 @@
+'use strict'
+
+var button, more, height, visible
+
+more = document.querySelector('.more')
+  //  Get height of .more element
+height = more.clientHeight
+  // Hide content with CSS
+more.style.cssText = 'max-height:0; overflow:hidden;'
+
+button = document.createElement('button')
+button.classList.add('show-more')
+button.innerHTML = 'Read more...'
+  // Insert button after .more element
+more.parentNode.insertBefore(button, more.nextSibling)
+
+button = document.querySelector('.show-more');
+
+visible = false
+
+button.addEventListener('click', function() {
+  // If visible is set to false
+  if (!visible) {
+    button.innerHTML = 'Show less...'
+    more.style.transition = 1 + 's'
+    more.style.maxHeight = height + 'px'
+  } else {
+    button.innerHTML = 'Read more...'
+    more.style.maxHeight = 0
+  }
+  // Alternate between false & true
+  visible = !visible
+})
